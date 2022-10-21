19d926f8d8b724620073ce6d7caaf6a79039b36b
Murad Gapurovich <darkkukri0550@gmail.com>
2022-10-22 00:20:42 +0300

    Реакт

---
+++ app/Http/Controllers/ExponentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Exponent;
+use App\Http\Requests\StoreExponentRequest;
+use App\Http\Requests\UpdateExponentRequest;
+
+class ExponentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreExponentRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreExponentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Exponent  $exponent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Exponent $exponent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Exponent  $exponent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Exponent $exponent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateExponentRequest  $request
+     * @param  \App\Models\Exponent  $exponent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateExponentRequest $request, Exponent $exponent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Exponent  $exponent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Exponent $exponent)
+    {
+        //
+    }
+}
